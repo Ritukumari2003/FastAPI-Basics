c217619945df6973fb2778d59230f6ca8bac9b2b
Ritukumari2003 <2503.ritu@gmail.com>
2026-07-16 11:59:11 +0530

    First API created

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Home Route 
 @app.get('/')
 def home():
-    return {'message': 'Hello World'}
+    return {'message': 'Welcome to Home Page'}
+
+# About Route
+@app.get('/about')
+def about():
+    return {'message': 'Welcome to About Page'}
+
+# Users Route
+@app.get('/users')
+def users():
+    return {
+        "users":['Mohit', 'Rohit', 'Amit'] 
+    }
