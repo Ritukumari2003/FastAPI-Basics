106f664f5710612777d3ff56c4b3db3bfbc1afdb
Ritukumari2003 <2503.ritu@gmail.com>
2026-07-16 15:04:20 +0530

    Path and Query Parameters

--- main.py
+++ main.py
@@ -12,9 +12,41 @@ def home():
 def about():
     return {'message': 'Welcome to About Page'}
 
-# Users Route
-@app.get('/users')
-def users():
+# # Users Route
+# @app.get('/users')
+# def users():
+#     return {
+#         "users":['Mohit', 'Rohit', 'Amit'] 
+#     }
+
+# Creating Dynamic URL 
+
+# Path Parameters
+# Users Route 
+@app.get('/users/{user_id}')
+def get_user(user_id: int):
     return {
-        "users":['Mohit', 'Rohit', 'Amit'] 
+        'user_id': user_id,
+    }
+
+# Single Query Parameters
+# Users Route 
+@app.get('/users')
+def get_user(name: str=None):    # To pass Null value 
+    return {
+        'Name': name,
+    }
+
+@app.get('/products')
+def get_user(limit: int=10):    # To pass Default value 
+    return {
+        'Limit': limit,
+    }
+
+# Multiple Query Parameters
+@app.get('/items')
+def get_user(name: str=None, price: int=0):    # To pass Default value 
+    return {
+        'Name': name,
+        'Price': price,
     }
